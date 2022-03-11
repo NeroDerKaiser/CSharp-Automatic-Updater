f307dab87fa1dfc758d8f8774dbc9f657a746990
Timo Steinbrech <46078258+NeroDerKaiser@users.noreply.github.com>
2022-03-11 12:46:49 +0100

    Add files via upload

---
+++ TestApp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestUpdate;
+
+namespace TestApp
+{
+    public partial class Form1 : Form, ITestUpdatable
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.label1.Text = ApplicationAssembly.GetName().Version.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public string ApplicationName
+        {
+            get { return "test"; }
+        }
+
+        public string ApplicationID
+        {
+            get { return "1"; }
+        }
+
+        public Assembly ApplicationAssembly
+        {
+            get { return Assembly.GetExecutingAssembly(); }
+        }
+
+        public Uri UpdateXmlLocation
+        {
+            get { return new Uri(""); }
+        }
+
+        public Form Context
+        {
+            get { return this; }
+        }
+
+        public Icon ApplicationIcon
+        {
+            get { return this.Icon; }
+        }
+    }
+}
---
+++ TestUpdate/Hasher.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TestUpdate
+{
+    public enum HashType
+    {
+        MD5,
+        SHA1,
+        SHA256,
+        SHA384,
+        SHA512,
+    }
+
+    public static class Hasher
+    {
+        public static string HashFile(string filePath, HashType algo)
+        {
+            switch (algo)
+            {
+                case HashType.MD5:
+                    return MakeHashString(MD5.Create().ComputeHash(new FileStream(filePath, FileMode.Open)));
+                    
+                case HashType.SHA1:
+                    return MakeHashString(SHA1.Create().ComputeHash(new FileStream(filePath, FileMode.Open)));
+                case HashType.SHA256:
+                    return MakeHashString(SHA256.Create().ComputeHash(new FileStream(filePath, FileMode.Open)));
+                case HashType.SHA384:
+                    return MakeHashString(SHA384.Create().ComputeHash(new FileStream(filePath, FileMode.Open)));
+                case HashType.SHA512:
+                    return MakeHashString(SHA512.Create().ComputeHash(new FileStream(filePath, FileMode.Open)));
+                default:
+                    return "";
+            }
+        }
+
+        private static string MakeHashString(byte[] hash)
+        {
+            StringBuilder s = new StringBuilder(hash.Length * 2);
+
+            foreach (byte b in hash)
+                s.Append(b.ToString("X2").ToLower());
+            return s.ToString();
+        }
+    }
+}
